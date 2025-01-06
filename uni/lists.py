c506fc420883d4e06ae026e519e50c7936ad5032
notarkhit <notarkhit@gmail.com>
2025-01-06 09:07:18 +0530

    python lecture

---
+++ uni/lists.py
@@ -0,0 +1,9 @@
+
+
+studentData = ['R23DE001',78,89,67,54,78.56,'CSA']
+
+print(type(studentData))
+print(type(studentData[0]))
+print(type(studentData[3]))
+print(type(studentData[4]))
+print(studentData)
