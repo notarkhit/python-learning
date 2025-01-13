ed30cca9bc275ef0f560e7da97ddbe9449147aec
notarkhit <notarkhit@gmail.com>
2025-01-13 08:54:50 +0530

    add ifelif.py

---
+++ uni/ifelif.py
@@ -0,0 +1,14 @@
+marks = int(input("Enter your marks: "))
+
+if marks > 90 :
+	print("You got 'A' Grade");
+elif marks > 75 :
+	print("You got 'B' Grade");
+elif marks > 65 :
+	print("You got 'C' Grade");
+elif marks > 55 :
+	print("You got 'D' Grade");
+elif marks > 45 :
+	print("You got 'E' Grade");
+else :
+	print("You got Fail Grade");
