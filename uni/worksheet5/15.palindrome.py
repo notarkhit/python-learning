bc3e72dbd03acb2d9345845e744a227c9d9043f6
notarkhit <notarkhit@gmail.com>
2025-01-28 09:35:58 +0530

    updates

---
+++ uni/worksheet5/15.palindrome.py
@@ -0,0 +1,9 @@
+
+userString = input ('Enter a string:  ')
+
+reversedUserString = userString[::-1]
+
+if userString == reversedUserString :
+    print('Palindrome')
+else:
+    print('not a Palindrome')
