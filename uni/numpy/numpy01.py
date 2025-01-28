1d05f2a44545806374fdaf43cdbcb75e8eedcae4
notarkhit <notarkhit@gmail.com>
2025-01-28 14:22:40 +0530

    update

--- uni/numpy/numpy01.py
+++ uni/numpy/numpy01.py
@@ -1,7 +1,8 @@
 import numpy as np
 
 import array as ar
+# array = ar.array('i', [1,2,3,4])
+# print(array)
+nArray = np.arange(1,10);
 
-array = ar.array('i', [1,2,3,4])
-
-print(array)
+print (nArray)
