aad202fc1348a11944a53cd68b21fae55b7f6588
notarkhit <notarkhit@gmail.com>
2025-03-04 14:05:23 +0530

    update

---
+++ functions/totalMarks.py
@@ -0,0 +1,9 @@
+# write a python program using function to find the total marks obtained by the students
+
+def totalMarks(srn, name , marks1, marks2, marks3, marks4, marks5):
+    print(srn)
+    print(name)
+    return (marks1+marks2+marks3+marks4+marks5)
+
+
+totalMarks(123,'name',12,2,4,6,7)
